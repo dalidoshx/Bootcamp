fd89cb5548af12ef8cbbee66ddf556d94878dcc1
Adrian Dancau <dalidoshx@Adrians-MacBook-Pro.local>
2024-07-29 16:51:22 +0300

    firt commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("Your nr is less than 0.5");
+// }
+// else if (random > 0.5) {
+//     console.log("Your nr is bigger then 0.5");
+// }
+
+// const dayOfWeek = 'Saturday';
+
+// if (dayOfWeek === 'Monday') {
+//     console.log("Uhhh I hate Mondays!") 
+// } else if (dayOfWeek === 'Saturday') {
+//     console.log("Yay I love Saturdays")
+// } else if (dayOfWeek === 'Friday') {
+//     console.log("Yay I love Fridays") 
+// } 
+
+//
+  

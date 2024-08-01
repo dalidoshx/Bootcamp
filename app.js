b9ec8bd67e1f74c7bbfdef70fe4ab34fe40ef6d8
Dalidoshx <dalidoshx@Adrians-MacBook-Pro.local>
2024-08-01 23:04:09 +0300

    update

--- app.js
+++ app.js
@@ -15,3 +15,48 @@
 // } else if (dayOfWeek === 'Friday') {
 //     console.log("Yay I love Fridays") 
 // } 
+
+
+// -- Nesting --
+// const password = prompt("please enter a new password");
+
+// //Password must be 6+ characters 
+// if (password.length >= 6) {
+//     if (password.indexOf(' ') === -1) {
+//         console.log("Good job! No space!")
+//     } else {
+//         console.log("Password cannot contain spaces!")
+//     }
+
+// } else {
+//     console.log("Password to short! Must be 6+ characters")
+// }
+// //Password cannnot include space
+
+// -- Logical operator && --
+// const password = prompt("Enter your password");
+
+// if (password.length >=6 && password.indexOf(" ") === -1) {
+//     console.log("Valid password!")
+// } else {
+//     console.log("Incorrect format for password!")
+// }
+ 
+// -- Logical operator || --
+// 0-5 free
+// 5-10 $10
+// 10-65 $20
+// 65+ free
+
+// const age = 90;
+// if (age >= 0 && age < 5 || age >= 65) {
+//     console.log("free")
+// } else if (age >= 5 && age < 10) {
+//     console.log("$10")
+// } else if (age >= 10 && age < 65) {
+//     console.log("$20") 
+// } else {
+//     console.log("Invalid age")
+// }
+
+// -- Logical operator ! --

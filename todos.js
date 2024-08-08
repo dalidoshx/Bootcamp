220cdd6af4873210eb913a1ae0f94a4bbb597523
dalidoshx <adi.dancau@gmail.com>
2024-08-08 20:19:42 +0300

    Sum arr

--- todos.js
+++ todos.js
@@ -1,7 +1,11 @@
-function isShortsWeather(temperature) {
-    if (temperature >= 75) {
-      return true;
-    } else {
-      return false;
-    }
+function sumArray(array) {
+    for (
+      var
+        index = 0,              // The iterator
+        length = array.length,  // Cache the array length
+        sum = 0;                // The total amount
+        index < length;         // The "for"-loop condition
+        sum += array[index++]   // Add number on each iteration
+    );
+    return sum;
   }
